b33adb3486b562305e2ef7e22bf4de833a2a5e2f
AlexPetrusca <petruscalex@gmail.com>
2024-09-24 14:30:36 -0700

    add bank option to heal action

--- src/actions/heal.py
+++ src/actions/heal.py
@@ -3,6 +3,11 @@ from src.util import robot
 
 
 class HealAction(Action):
+    bank = False
+
+    def __init__(self, bank=False):
+        self.bank = bank
+
     def first_tick(self):
         self.set_status("Routing to Healer...")
 
@@ -17,7 +22,14 @@ class HealAction(Action):
             robot.right_click(805, 520)  # right click healer
         if self.tick_counter == Action.sec2tick(13):
             robot.click(730, 568)  # heal
-        return self.tick_counter == Action.sec2tick(14)
+        if self.bank:
+            if self.tick_counter == Action.sec2tick(15):
+                robot.click(1080, 524)  # click bank chest
+            if self.tick_counter == Action.sec2tick(19):
+                robot.click(1074, 110)  # close bank
+            return self.tick_counter == Action.sec2tick(20)
+        else:
+            return self.tick_counter == Action.sec2tick(14)
 
     def last_tick(self):
         pass
--- src/bot.py
+++ src/bot.py
@@ -36,7 +36,7 @@ class Bot:
 
         self.action_queue.append(PickUpItemsAction())
         self.action_queue.append(HomeTeleportAction())
-        self.action_queue.append(HealAction())  # todo: a heal action should probably include a home teleport action
+        self.action_queue.append(HealAction(bank=True))  # todo: a heal action should probably include a home teleport action
 
         # Slayer
         # self.action_queue.append(SlayerAction())
