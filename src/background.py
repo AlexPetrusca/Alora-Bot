c07e30083d137d0761002ad8f678056340804a91
AlexPetrusca <petruscalex@gmail.com>
2024-10-01 13:53:03 -0700

    add requirements.txt + add logic to wait for server startup

--- src/background.py
+++ src/background.py
@@ -1,6 +1,5 @@
+import requests
 from requests import request
-
-from src.actions.action import Action
 
 TICK_INTERVAL = 0.1  # 100ms tick
 KEYLOGGER_HOST = "http://localhost:8080"
@@ -17,13 +16,11 @@ class BackgroundScript:
 
     def tick(self, t):
         if t - self.t_last > TICK_INTERVAL:
-            res = request("GET", f"{KEYLOGGER_HOST}/keyStrokes")
-            if res.ok:
-                for key in res.json()['keys']:
-                    if key in self.key_toggles:
-                        self.untoggle_key(key)
-                    else:
-                        self.toggle_key(key)
+            for key in BackgroundScript.get_key_strokes():
+                if key in self.key_toggles:
+                    self.untoggle_key(key)
+                else:
+                    self.toggle_key(key)
             self.t_last = t
 
     def toggle_key(self, key):
@@ -40,4 +37,12 @@ class BackgroundScript:
         if type(key) is not int:
             key = key.value
         return key in self.key_toggles
+
+    @staticmethod
+    def get_key_strokes():
+        try:
+            res = request("GET", f"{KEYLOGGER_HOST}/keyStrokes")
+            return res.json()['keys'] if res.ok else []
+        except requests.exceptions.ConnectionError:
+            return []
         
